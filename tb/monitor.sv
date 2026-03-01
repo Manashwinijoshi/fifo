97476423c1ab329245de36a4f6d005b33015dfcf
Manashwinijoshi <93579052+Manashwinijoshi@users.noreply.github.com>
2026-03-01 20:23:05 +0530

    Create monitor.sv

---
+++ tb/monitor.sv
@@ -0,0 +1,30 @@
+class monitor;
+
+  transaction tx;
+  virtual inf.MON vinf;
+  mailbox#(transaction) mon2sb;
+
+  function new(mailbox#(transaction) mon2sb, virtual inf.MON vinf);
+    this.mon2sb=mon2sb;
+    this.vinf=vinf;
+  endfunction
+
+  task run();
+   forever begin
+    
+     @(vinf.mon_cb)
+      tx=new();
+       tx.wr_en = vinf.mon_cb.wr_en;
+       tx.rd_en = vinf.mon_cb.rd_en;
+       tx.d_in = vinf.mon_cb.d_in;
+       tx.d_out = vinf.mon_cb.d_out;
+       tx.full = vinf.mon_cb.full;
+       tx.empty = vinf.mon_cb.empty;
+     mon2sb.put(tx);
+     
+   end
+
+  endtask
+
+  
+endclass
