4e41d3676453d404c49e9c8e6aaf904b453ae3df
Manashwinijoshi <93579052+Manashwinijoshi@users.noreply.github.com>
2026-03-01 19:15:46 +0530

    Create write_seq.sv

---
+++ tb/write_seq.sv
@@ -0,0 +1,15 @@
+class write_seq extends base_sequence;
+  function new(mailbox#(transaction) seq2drv);
+    super.new(seq2drv);
+  endfunction
+  task run();
+      transaction tx;
+    repeat(8)
+      begin
+        tx = new();
+        assert (tx.randomize() with {wr_en == 1; rd_en == 0;}) else
+        $fatal(1, "Randomization failed in write_seq");
+          seq2drv.put(tx);
+      end
+  endtask
+endclass
