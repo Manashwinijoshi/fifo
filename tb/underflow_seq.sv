e9e6bbca5c110ce060399f636e74a98d390e66db
Manashwinijoshi <93579052+Manashwinijoshi@users.noreply.github.com>
2026-03-02 18:41:24 +0530

    Update underflow_seq.sv

--- tb/underflow_seq.sv
+++ tb/underflow_seq.sv
@@ -1,18 +1,30 @@
 class underflow_seq extends base_sequence;
-  function new(mailbox#(transaction) seq2drv);
-    super.new(seq2drv);
+
+  function new(mailbox #(transaction) seq2drv,
+               ref event seq_done);
+    super.new(seq2drv, seq_done);
   endfunction
+
+
   task run();
-      transaction tx;
-    repeat(8)
-      begin
-        tx = new();
-        assert (tx.randomize() with {
-          wr_en dist {1 := 10, 0 := 90};
-          rd_en dist {0 := 20, 1 := 80};
-      }) else
-          $fatal(1, "Randomization failed in underflow_seq");
-          seq2drv.put(tx);
-      end
+    transaction tx;
+
+    repeat (`TX) begin
+      tx = new();
+
+      assert (tx.randomize() with {
+        wr_en dist {1 := 10, 0 := 90};
+        rd_en dist {1 := 80, 0 := 20};
+      })
+      else
+        $fatal(1, "Randomization failed in underflow_seq");
+
+      seq2drv.put(tx);
+    end
+
+    // ⭐ Notify sequence completion
+    -> seq_done;
+
   endtask
+
 endclass
