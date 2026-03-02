d10e136574b42e8092cba23e2cfc0089ecdbb6f7
Manashwinijoshi <93579052+Manashwinijoshi@users.noreply.github.com>
2026-03-02 10:49:07 +0530

    Update driver.sv

--- tb/driver.sv
+++ tb/driver.sv
@@ -1,19 +1,13 @@
 class driver;
 
   transaction tx;
-
   mailbox#(transaction) seq2drv;
-  mailbox#(transaction) drv2sb;
-
   virtual inf.DRV vinf;
-
   function new(
     mailbox#(transaction) seq2drv,
-    mailbox#(transaction) drv2sb,
     virtual inf.DRV vinf
   );
     this.seq2drv = seq2drv;
-    this.drv2sb  = drv2sb;
     this.vinf    = vinf;
   endfunction
 
@@ -22,14 +16,12 @@ class driver;
       seq2drv.get(tx);
       @(vinf.drv_cb);
       drive();
-      drv2sb.put(tx);
     end
   endtask
 
   task drive();
     vinf.drv_cb.wr_en <= tx.wr_en;
     vinf.drv_cb.rd_en <= tx.rd_en;
-
     if(tx.wr_en)
       vinf.drv_cb.d_in <= tx.d_in;
   endtask
