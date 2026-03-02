819eb7e67ec4de9514e7ed1324c0a86b51d3359a
Manashwinijoshi <93579052+Manashwinijoshi@users.noreply.github.com>
2026-03-02 18:44:26 +0530

    Update base_sequence.sv

--- tb/base_sequence.sv
+++ tb/base_sequence.sv
@@ -1,9 +1,11 @@
 class base_sequence;
 
   mailbox #(transaction) seq2drv;
-
-  function new(mailbox #(transaction) seq2drv);
+   event seq_done;
+  function new(mailbox #(transaction) seq2drv, ref event seq_done);
     this.seq2drv = seq2drv;
+    this.seq_done = seq_done;
+
   endfunction
 
   virtual task run();
