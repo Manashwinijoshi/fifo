0feb3fca2091bb391bfd0e7e18e36cec559a899c
Manashwinijoshi <93579052+Manashwinijoshi@users.noreply.github.com>
2026-03-02 18:37:47 +0530

    Update test.sv

--- tb/test.sv
+++ tb/test.sv
@@ -1,19 +1,36 @@
-class test;
+`define TX 8
+`include "interface.sv"
+`include "transaction.sv"
+`include "base_sequence.sv"
+`include "mix_seq.sv"
+`include "write_seq.sv"
+`include "underflow_seq.sv"
+`include "driver.sv"
+`include "monitor.sv"
+`include "scoreboard.sv"
+`include "environment.sv"
 
+class test;
+  event done,sb_done;
   environment env;
-  write_seq seq;
+  //write_seq seq;
+  underflow_seq seq;
 
   function new(virtual inf vif);
-    env = new(vif);
-    seq = new(env.seq2drv);
+    env = new(vif,sb_done);
+    seq = new(env.seq2drv,done);
   endfunction
+
   task run();
      fork
        env.run();
+       seq.run();
      join_none
-    seq.run();
-    #1000;
-    env.sb.report();
-    $finish;
+  
+  
+ @done;
+ @sb_done;
+  env.sb.report();
+  $finish;
   endtask
 endclass
