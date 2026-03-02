1f2c04b7e5063be8f2884b62b68dd60e197088f0
Manashwinijoshi <93579052+Manashwinijoshi@users.noreply.github.com>
2026-03-02 18:40:15 +0530

    Update mix_seq.sv

--- tb/mix_seq.sv
+++ tb/mix_seq.sv
@@ -1,15 +1,18 @@
 class mix_seq extends base_sequence;
-  function new(mailbox#(transaction) seq2drv);
-    super.new(seq2drv);
+  function new(mailbox#(transaction) seq2drv, ref event seq_done);
+    super.new(seq2drv,seq_done);
   endfunction
   task run();
       transaction tx;
-    repeat(8)
+    repeat(`TX)
       begin
         tx = new();
         assert (tx.randomize()) else
           $fatal(1, "Randomization failed in mix_seq");
-          seq2drv.put(tx);
+        
+
+  seq2drv.put(tx);
       end
+  ->seq_done;
   endtask
 endclass
