c2df114fb54d1df8e72e9d293d01e4f0adca882f
Manashwinijoshi <93579052+Manashwinijoshi@users.noreply.github.com>
2026-03-02 18:38:15 +0530

    Update monitor.sv

--- tb/monitor.sv
+++ tb/monitor.sv
@@ -2,29 +2,30 @@ class monitor;
 
   transaction tx;
   virtual inf.MON vinf;
-  mailbox#(transaction) mon2sb;
+  mailbox #(transaction) mon2sb;
 
-  function new(mailbox#(transaction) mon2sb, virtual inf.MON vinf);
-    this.mon2sb=mon2sb;
-    this.vinf=vinf;
+  function new(mailbox #(transaction) mon2sb,
+               virtual inf.MON vinf);
+    this.mon2sb = mon2sb;
+    this.vinf   = vinf;
   endfunction
 
   task run();
-   forever begin
-    
-     @(vinf.mon_cb)
-      tx=new();
-       tx.wr_en = vinf.mon_cb.wr_en;
-       tx.rd_en = vinf.mon_cb.rd_en;
-       tx.d_in = vinf.mon_cb.d_in;
-       tx.d_out = vinf.mon_cb.d_out;
-       tx.full = vinf.mon_cb.full;
-       tx.empty = vinf.mon_cb.empty;
-     mon2sb.put(tx);
-     
-   end
+    forever begin
+      @(vinf.mon_cb);
 
+      tx = new();
+
+      // Sample control + input signals immediately
+      tx.wr_en = vinf.mon_cb.wr_en;
+      tx.rd_en = vinf.mon_cb.rd_en;
+      tx.d_in  = vinf.mon_cb.d_in;
+      tx.full  = vinf.mon_cb.full;
+      tx.empty = vinf.mon_cb.empty;
+      tx.d_out = vinf.mon_cb.d_out;
+
+      mon2sb.put(tx);
+    end
   endtask
 
-  
 endclass
