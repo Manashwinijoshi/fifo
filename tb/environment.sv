d35499a5e9146dcb714e99d4f217c42da8481839
Manashwinijoshi <93579052+Manashwinijoshi@users.noreply.github.com>
2026-03-03 14:38:28 +0530

    Update environment.sv

--- tb/environment.sv
+++ tb/environment.sv
@@ -2,28 +2,33 @@ class environment;
   driver drv;
   monitor mon;
   scoreboard sb;
-  event sb_done;
+  sequencer sqr;
+  
+  event next_tr;
   
   mailbox#(transaction) seq2drv;
   mailbox#(transaction) mon2sb;
 
   virtual inf vinf;
 
-  function new(virtual inf vinf,event sb_done);
+  function new(virtual inf vinf,event next_tr);
     this.vinf = vinf ;
-    this.sb_done = sb_done;
+    this.next_tr = next_tr;
     seq2drv = new();
     mon2sb = new();
-    drv = new(seq2drv,vinf);
-    mon = new(mon2sb,vinf);
-    sb = new(mon2sb,sb_done);
+    drv = new(seq2drv,vinf,next_tr);
+    mon = new(mon2sb,vinf,next_tr);
+    sb = new(mon2sb);
+    sqr = new(seq2drv,"write_seq",8,next_tr);
   endfunction 
 
   task run();
    fork 
+     sqr.run();
      drv.run();
      mon.run();
      sb.run();
+     
   join_none
   endtask
 endclass
