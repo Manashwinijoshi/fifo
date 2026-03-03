cd016c1158de248d9d7a6338833de48e09480451
Manashwinijoshi <93579052+Manashwinijoshi@users.noreply.github.com>
2026-03-03 15:36:34 +0530

    Update test.sv

--- tb/test.sv
+++ tb/test.sv
@@ -1,13 +1,3 @@
-`include "interface.sv"
-`include "transaction.sv"
-`include "base_sequence.sv"
-`include "mix_seq.sv"
-`include "write_seq.sv"
-`include "underflow_seq.sv"
-`include "driver.sv"
-`include "monitor.sv"
-`include "scoreboard.sv"
-`include "environment.sv"
 
 class test;
   event next_tr;
