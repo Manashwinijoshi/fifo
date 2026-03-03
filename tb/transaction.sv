97ea0322bf3b3b5388c366ce00ecfbbb752380be
Manashwinijoshi <93579052+Manashwinijoshi@users.noreply.github.com>
2026-03-03 13:11:31 +0530

    Update transaction.sv

--- tb/transaction.sv
+++ tb/transaction.sv
@@ -7,5 +7,9 @@ class transaction;
    logic full;
    logic empty;
 
+   function void display(string parent="");
+          $display("[%s] wr=%0b rd=%0b din=%0h dout=%0h full=%0b empty=%0b",
+            parent, wr_en, rd_en, d_in, d_out, full, empty);
+   endfunction
 
 endclass
