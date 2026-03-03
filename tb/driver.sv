434d454ef6fca5e91db4cc2b542770397e18f12a
Manashwinijoshi <93579052+Manashwinijoshi@users.noreply.github.com>
2026-03-03 14:24:39 +0530

    Update driver.sv

--- tb/driver.sv
+++ tb/driver.sv
@@ -15,27 +15,15 @@ class driver;
     this.next_tr = next_tr;
   endfunction
 
- task run();
-  forever begin
-    wait(!vinf.reset);
-    seq2drv.get(tx);
-    drive();
-    -> next_tr;
-    @(vif.drv_cb);
-  end
-endtask
-
-  task drive();
-    vinf.drv_cb.wr_en <= tx.wr_en;
-    vinf.drv_cb.rd_en <= tx.rd_en;
-
-    if(tx.wr_en)
-       vinf.drv_cb.d_in <= tx.d_in;
-    
-  @(vinf.drv_cb);          // wait one cycle
-  vinf.drv_cb.wr_en <= 0;  // deassert after driving
-  vinf.drv_cb.rd_en <= 0;
-  vinf.drv_cb.d_in  <='0;
-  endtask
+	task run();
+		forever begin
+      seq2drv.get(tx);
+			vinf.drv_cb.rd_en<=tx.rd_en;
+			vinf.drv_cb.wr_en<=tx.wr_en;
+			vinf.drv_cb.d_in<=tx.d_in;
+			->next_tr;
+      @(vinf.drv_cb);
+		end
+	endtask:run
 
 endclass
