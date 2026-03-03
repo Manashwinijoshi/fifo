5198bbf040ddad5ff2c6e7aa8ced4022ebee3e75
Manashwinijoshi <93579052+Manashwinijoshi@users.noreply.github.com>
2026-03-03 13:58:33 +0530

    Update driver.sv

--- tb/driver.sv
+++ tb/driver.sv
@@ -3,6 +3,7 @@ class driver;
   transaction tx;
   mailbox#(transaction) seq2drv;
   virtual inf vinf;
+
   function new(
     mailbox#(transaction) seq2drv,
     virtual inf.DRV vinf
@@ -11,22 +12,26 @@ class driver;
     this.vinf    = vinf;
   endfunction
 
-  task run();
-    forever begin
-      seq2drv.get(tx);
-      @(vinf.drv_cb);
-      drive();
-    end
-  endtask
+ task run();
+  wait(!vinf.reset);
+
+  forever begin
+    seq2drv.get(tx);
+    drive();
+  end
+endtask
 
   task drive();
     vinf.drv_cb.wr_en <= tx.wr_en;
-  vinf.drv_cb.rd_en <= tx.rd_en;
-  if(tx.wr_en)
-    vinf.drv_cb.d_in <= tx.d_in;
+    vinf.drv_cb.rd_en <= tx.rd_en;
+
+    if(tx.wr_en)
+       vinf.drv_cb.d_in <= tx.d_in;
+    
   @(vinf.drv_cb);          // wait one cycle
   vinf.drv_cb.wr_en <= 0;  // deassert after driving
   vinf.drv_cb.rd_en <= 0;
+  vinf.drv_cb.d_in  <='0;
   endtask
 
 endclass
