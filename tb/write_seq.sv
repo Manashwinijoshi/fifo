cf465dc9509f58bbd047d75e1ce418d8b7be39fd
Manashwinijoshi <93579052+Manashwinijoshi@users.noreply.github.com>
2026-03-03 13:23:01 +0530

    Update write_seq.sv

--- tb/write_seq.sv
+++ tb/write_seq.sv
@@ -1,6 +1,6 @@
 class write_seq extends base_sequence;
-  function new(mailbox #(transaction) seq2drv, ref event seq_done);
-    super.new(seq2drv, seq_done);
+  function new(mailbox #(transaction) seq2drv);
+    super.new(seq2drv);
   endfunction
 
   task run();
@@ -9,9 +9,9 @@ class write_seq extends base_sequence;
       begin
         tx = new();
         assert (tx.randomize() with {wr_en == 1; rd_en == 0;}) else
-        $fatal(1, "Randomization failed in write_seq");
+                  $fatal(1, "Randomization failed in write_seq");
+        
           seq2drv.put(tx);
       end
-   -> seq_done;  
-  endtask
+   endtask
 endclass
